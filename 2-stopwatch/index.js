9d0a198589006ede211d2020bf7b5581b12e7a1d
Angel Soto <100176750+Angeltheesoto@users.noreply.github.com>
2022-07-04 13:12:18 -0400

    Added final touches, color, hours, and sizing.

--- 2-stopwatch/index.js
+++ 2-stopwatch/index.js
@@ -4,6 +4,7 @@ const start = document.querySelector('.start');
 const pause = document.querySelector('.stop');
 const reset = document.querySelector('.reset');
 let int = null;
+let hour = 0;
 let minute = 0;
 let second = 0;
 
@@ -22,9 +23,10 @@ pause.addEventListener('click', () => {
 // Resets min/ sec to 0, html text to 00:00, and the interval back to null.
 reset.addEventListener('click', () => {
  clearInterval(int)
+ hour = 0;
  minute = 0;
  second = 0;
- timer.innerHTML = ` 00 : 00 `
+ timer.innerHTML = ` 00 : 00 : 00 `
 })
 // Runs seconds to 60 then adds 1 to minutes.
 function timing() {
@@ -35,12 +37,17 @@ function timing() {
  }
  if(minute === 60){
   minute = 0;
+  hour += 1;
+ }
+ if(hour === 24){
+  hour = 0;
  }
  // Adds 0 in front if number is less than 10.
  // Ternary js = if - else if - return
+ let h = hour < 10 ? '0' + hour : hour;
  let m = minute < 10 ? '0' + minute : minute;
  let s = second < 10 ? '0' + second : second;
- timer.textContent = ` ${m} : ${s} `;
+ timer.textContent = ` ${h} : ${m} : ${s} `;
 }
 
 
