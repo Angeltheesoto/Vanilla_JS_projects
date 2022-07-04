9375a1f1ccd01013d419eec52f8dbdb0ebe570fe
Angel Soto <100176750+Angeltheesoto@users.noreply.github.com>
2022-07-04 15:46:56 -0400

    Finished date, working on current time

--- 3-clock_date/index.js
+++ 3-clock_date/index.js
@@ -1,19 +1,68 @@
 
 // DATE -
+// variables
+const weekday = document.querySelector('.weekday');
+const month = document.querySelector('.month');
+const day = document.querySelector('.day');
+const year = document.querySelector('.year');
 
+let daysOfWeek = [
+ 'Sunday',
+ 'Monday',
+ 'Tuesday',
+ 'Wednesday',
+ 'Thursday',
+ 'Friday',
+ 'Saturday'
+];
 
+let months = [
+ 'January',
+ 'febuary',
+ 'March',
+ 'April',
+ 'May',
+ 'June',
+ 'July',
+ 'August',
+ 'September',
+ 'October',
+ 'November',
+ 'December'
+];
 
+let date = new Date;
+let dd = date.getDay(); // Day of the week
+let mm = date.getMonth(); // Month
+let nn = date.getDate(); // Number of the month day
+let yy = date.getFullYear(); // Year
+let currentDay = daysOfWeek[dd];
+let currentMonth = months[mm];
 
-
-
-
-
-
-
-
-
+function todayDate() {
+ weekday.innerHTML = `${currentDay},`;
+ month.innerHTML = `${currentMonth}`;
+ day.innerHTML = `${nn},`;
+ year.innerHTML = `${yy}`;
+};
+todayDate();
 
 
 // TIME -
+//  variables
+const time = document.querySelector('.time');
+let nycTime = date.toLocaleTimeString();
+
+function currentTime() {
+ time.innerHTML = nycTime;
+};
+currentTime();
+
+let myTime = setTimeout()
+
+
+
+
+
 
 
